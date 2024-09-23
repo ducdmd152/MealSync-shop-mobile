474b90d5f73a0a67857c4d2948eaf1c9d4ef3824
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-09-23 16:43:15 +0700

    Basic main shop layout

--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -1,10 +1,134 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { ReactNode } from "react";
+import AvatarImage from "react-native-paper/lib/typescript/components/Avatar/AvatarImage";
+import { Avatar, Switch } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+import { Link, router } from "expo-router";
+const redirections = {
+  shop: [
+    {
+      text: "Hồ sơ cửa hàng",
+      icon: <Ionicons size={20} name="storefront-outline" />,
+      handlePress: () => router.push("/shop/profile"),
+      textStyleClasses: "text-gray-800",
+    },
+    {
+      text: "Hiệu suất bán hàng",
+      icon: <Ionicons size={20} name="stats-chart-outline" />,
+      handlePress: () => router.push("/shop/statistics"),
+      textStyleClasses: "text-gray-800",
+    },
+    {
+      text: "Cài đặt cửa hàng",
+      icon: <Ionicons size={20} name="settings-outline" />,
+      handlePress: () => router.push("/shop/settings"),
+      textStyleClasses: "text-gray-800",
+    },
+  ] as LinkItem[],
+  account: [
+    {
+      text: "Tài khoản của tôi",
+      icon: <Ionicons size={20} name="person-circle-outline" />,
+      handlePress: () => router.push("/account"),
+      textStyleClasses: "text-gray-800",
+    },
+    {
+      text: "Đăng xuất",
+      icon: <Ionicons size={20} name="log-out-outline" />,
+      handlePress: () => router.push("/logout"),
+      textStyleClasses: "text-gray-600",
+    },
+  ] as LinkItem[],
+};
 const Shop = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(true);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   return (
-    <View>
-      <Text>Welcome to shop</Text>
+    <View className="w-full h-full bg-white text-black p-4 relative">
+      <TouchableOpacity className="flex-row justify-between items-center gap-x-2 pb-4">
+        <View className="flex-row justify-start items-center gap-x-2">
+          <Avatar.Image
+            size={48}
+            source={{
+              uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV5-FEuyxb-HMUB41PwAEX_yopAjz0KgMAbg&s",
+            }}
+          />
+          <Text className="text-lg italic text-gray text-primary font-pmedium ">
+            Tiệm ăn tháng năm
+          </Text>
+        </View>
+        <Switch
+          color="#e95137"
+          value={isSwitchOn}
+          onValueChange={onToggleSwitch}
+        />
+      </TouchableOpacity>
+      <View className="border-b-2 border-gray-300"></View>
+
+      <View className="gap-y-[24px] mt-2 pb-[120px] px-1">
+        <LinkGroup
+          title="CỬA HÀNG"
+          data={redirections.shop}
+          containerStyleClasses="mt-4"
+        />
+        <LinkGroup
+          title="TÀI KHOẢN"
+          data={redirections.account}
+          containerStyleClasses="mt-4"
+        />
+      </View>
+    </View>
+  );
+};
+
+interface LinkItem {
+  text: string;
+  icon: ReactNode;
+  handlePress: () => void;
+
+  textStyleClasses: string;
+}
+
+interface LinkGroupProps {
+  title: string;
+  containerStyleClasses: string;
+  data: LinkItem[];
+}
+
+const LinkGroup: React.FC<LinkGroupProps> = ({
+  title,
+  containerStyleClasses = "",
+  data,
+}) => {
+  return (
+    <View className={containerStyleClasses}>
+      <Text className="font-psemibold text-gray-600">{title}</Text>
+      <View className="text-gray-700">
+        {data.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => item.handlePress()}
+            className="flex-row p-1 justify-between items-center mt-1"
+          >
+            <View className="flex-row gap-x-2">
+              {item.icon}
+              <Text
+                className={`font-psemibold text-md text-gray-700 ${item.textStyleClasses}`}
+              >
+                {item.text}
+              </Text>
+            </View>
+            <Ionicons size={20} name="chevron-forward-outline" />
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
