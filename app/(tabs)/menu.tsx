48012f3c63b948f7a9cd8c5be5baeacd555272ce
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-10-04 23:58:43 +0700

    Menu Tabs

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -5,190 +5,39 @@ import { Ionicons } from "@expo/vector-icons";
 import { Searchbar } from "react-native-paper";
 import Collapsible from "react-native-collapsible";
 import { Tab } from "react-native-elements";
+import MenuMainItems from "@/components/menu/MenuMainItems";
+import MenuGroupOptions from "@/components/menu/MenuGroupOptions";
 
 const Menu = () => {
   const [index, setIndex] = React.useState(0);
-  const [searchQuery, setSearchQuery] = React.useState("");
-  const [isCollapsibles, setIsCollapsibles] = React.useState<boolean[]>(
-    Array(5).fill(true)
-  );
-  useEffect(() => {
-    setIsCollapsibles(Array(5).fill(false));
-  }, []);
   return (
-    <View className="w-full h-full bg-white text-black  relative">
-      <View className="absolute w-full items-center justify-center bottom-1 left-0 z-10">
-        <CustomButton
-          title="Thêm món/danh mục mới"
-          handlePress={() => {}}
-          containerStyleClasses="w-[98%] h-[44px] px-4 bg-transparent border-2 border-gray-200 bg-secondary-100 font-psemibold z-10"
-          // iconLeft={
-          //   <Ionicons name="add-circle-outline" size={21} color="white" />
-          // }
-          textStyleClasses="text-[14px] text-gray-900 ml-1 text-white"
-        />
-      </View>
-
-      <View className="w-full gap-2 p-4">
-        <View className="mt-4">
-          <Tab
-            value={index}
-            onChange={setIndex}
-            indicatorStyle={{
-              backgroundColor: "white",
-              height: 1,
+    <View className="bg-white">
+      <View className="p-2 pb-0">
+        <View className="flex-row items-center justify-center border-2 border-gray-200 rounded-md">
+          <CustomButton
+            title="Thực đơn chính"
+            handlePress={() => {
+              setIndex(0);
             }}
-          >
-            <Tab.Item>
-              <Text className="text-gray-900 text-md">Tab</Text>
-            </Tab.Item>
-            <Tab.Item>Tab</Tab.Item>
-          </Tab>
-        </View>
-        {/* <View className="w-full">
-          <Searchbar
-            style={{
-              height: 50,
-              // backgroundColor: "white",
-              // borderColor: "lightgray",
-              // borderWidth: 2,
-            }}
-            inputStyle={{ minHeight: 0 }}
-            placeholder="Nhập tên món..."
-            onChangeText={setSearchQuery}
-            value={searchQuery}
+            containerStyleClasses={`flex-1 px-2  h-[40px] rounded-md ${
+              index == 0 ? "bg-primary-100" : "bg-white"
+            }`}
+            textStyleClasses={`text-sm ${index == 0 ? "text-white" : ""}`}
           />
-        </View> */}
-        <ScrollView style={{ width: "100%", flexShrink: 0 }} horizontal={true}>
-          <View className="w-full flex-row gap-2 items-center justify-between pb-2">
-            <Text className="bg-gray-100 rounded-xl px-4 py-2 bg-secondary">
-              Tất cả
-            </Text>
-            <Text className="bg-gray-100 rounded-xl px-4 py-2 ">
-              Trong khung giờ
-            </Text>
-            <Text className="bg-gray-100 rounded-xl px-4 py-2 ">
-              Ngoài khung giờ
-            </Text>
-            <Text className="bg-gray-100 rounded-xl px-4 py-2 ">
-              Tạm hết hàng
-            </Text>
-            <Text className="bg-gray-100 rounded-xl px-4 py-2 ">Đã ẩn</Text>
-          </View>
-        </ScrollView>
-        <ScrollView style={{ width: "100%", flexGrow: 1 }}>
-          <View className="gap-y-2 pb-[154px]">
-            {Array.from({ length: 3 }, (_, i) => (
-              <View key={i}>
-                <TouchableOpacity
-                  className="flex-row justify-between items-center pr-2 mb-2"
-                  onPress={() =>
-                    setIsCollapsibles(
-                      isCollapsibles.map((item, index) =>
-                        index === i ? !item : item
-                      )
-                    )
-                  }
-                >
-                  <View>
-                    <View className="flex-row items-center gap-x-2">
-                      <Text className="font-bold text-lg text-gray-800">
-                        {i === 0 ? "Ăn sáng" : i === 1 ? "Ăn trưa" : "Ăn tối"}
-                      </Text>
-                      <Text className="text-gray-700 text-sm">2 món</Text>
-                    </View>
-                    <CustomButton
-                      title="Chỉnh sửa danh mục"
-                      handlePress={() => {}}
-                      containerStyleClasses="bg-white border-gray-200 border-2 h-[26px] px-[8px]"
-                      textStyleClasses="text-gray-700 text-[11px] mt-[-5px] text-[#227B94]"
-                      // iconRight={
-                      //   <View className="ml-1">
-                      //     <Ionicons
-                      //       name="create-outline"
-                      //       size={14}
-                      //       color="#227B94"
-                      //     />
-                      //   </View>
-                      // }
-                    />
-                  </View>
-                  <View className="flex-row items-center gap-x-6">
-                    {isCollapsibles[i] ? (
-                      <Ionicons
-                        name="chevron-up-outline"
-                        size={21}
-                        color="gray"
-                      />
-                    ) : (
-                      <Ionicons
-                        name="chevron-down-outline"
-                        size={21}
-                        color="gray"
-                      />
-                    )}
-                  </View>
-                </TouchableOpacity>
-
-                <Collapsible collapsed={isCollapsibles[i]}>
-                  <View className="gap-y-2 pb-2">
-                    {Array.from({ length: 2 }, (_, j) => (
-                      <View
-                        key={j}
-                        className="p-4 pt-3 bg-white border-2 border-gray-300 rounded-lg"
-                      >
-                        <View className="flex-row items-start justify-between gap-2">
-                          <View className="flex-row justify-start items-start gap-2">
-                            <Image
-                              source={{
-                                uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/C%C6%A1m_T%E1%BA%A5m%2C_Da_Nang%2C_Vietnam.jpg/1200px-C%C6%A1m_T%E1%BA%A5m%2C_Da_Nang%2C_Vietnam.jpg",
-                              }}
-                              resizeMode="cover"
-                              className="h-[36px] w-[40px] rounded-md opacity-85"
-                            />
-                            <View>
-                              <Text className="text-md font-psemibold mt-[-2px]">
-                                {j === 0 ? "Cơm tấm Sài Gòn" : "Phở bò"}
-                              </Text>
-                              <Text className="text-md italic text-gray-500 mt-[-2px]">
-                                {j === 0 ? "120.000đ" : "80.000đ"}
-                              </Text>
-                            </View>
-                          </View>
-
-                          <View className="flex-row gap-2 items-start">
-                            <Text className="bg-blue-100 text-blue-800 text-[12.5px] font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                              Status
-                            </Text>
-                          </View>
-                        </View>
-                        <View className="flex-row justify-end gap-2 pt-4">
-                          <TouchableOpacity
-                            onPress={() => {}}
-                            className="bg-[#227B94] border-[#227B94] border-2 rounded-md items-center justify-center px-[6px] py-[2.2px]"
-                          >
-                            <Text className="text-[13.5px] text-white">
-                              Chỉnh sửa
-                            </Text>
-                          </TouchableOpacity>
-                          {/* <TouchableOpacity
-                            onPress={() => {}}
-                            className="bg-white border-[#227B94] border-2 rounded-md items-center justify-center px-[6px] py-[2.2px]"
-                          >
-                            <Text className="text-[13.5px]">Chi tiết</Text>
-                          </TouchableOpacity> */}
-                        </View>
-                      </View>
-                    ))}
-                  </View>
-                </Collapsible>
-
-                <View className="border-b-2 border-gray-200"></View>
-              </View>
-            ))}
-          </View>
-        </ScrollView>
+          <View className="w-[4px]"></View>
+          <CustomButton
+            title="Lựa chọn phụ"
+            handlePress={() => {
+              setIndex(1);
+            }}
+            containerStyleClasses={`flex-1 px-2  h-[40px] rounded-md ${
+              index == 1 ? "bg-primary-100" : "bg-white"
+            }`}
+            textStyleClasses={`text-sm ${index == 1 ? "text-white" : ""}`}
+          />
+        </View>
       </View>
+      {index == 0 ? <MenuMainItems /> : <MenuGroupOptions />}
     </View>
   );
 };
