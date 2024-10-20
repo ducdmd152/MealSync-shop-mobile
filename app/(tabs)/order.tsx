04657b8dfb09d62bd0fa610a5b1d066f9fa9303e
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-10-20 20:30:53 +0700

    Load order list api 80% (filter not yet)

--- app/(tabs)/order.tsx
+++ app/(tabs)/order.tsx
@@ -1,7 +1,7 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 import CustomButton from "@/components/custom/CustomButton";
-import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Searchbar } from "react-native-paper";
 import { Ionicons } from "@expo/vector-icons";
 import PagingRequestQuery from "@/types/queries/PagingRequestQuery";
 import useFetchWithRQWithFetchFunc from "@/hooks/fetching/useFetchWithRQWithFetchFunc";
@@ -15,19 +15,38 @@ import OrderFetchModel, {
 import apiClient from "@/services/api-services/api-client";
 import { endpoints } from "@/services/api-services/api-service-instances";
 import sessionService from "@/services/session-service";
-
+const formatTime = (time: number): string => {
+  const hours = Math.floor(time / 100)
+    .toString()
+    .padStart(2, "0");
+  const minutes = (time % 100).toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+};
 interface OrderFetchQuery extends PagingRequestQuery {
   status: number[];
   id: string;
   phoneNumber: string;
-  startTime?: number;
-  endTime?: number;
-  intendedRecieveDate?: string;
+  startTime: number;
+  endTime: number;
+  intendedRecieveDate: string;
 }
 
 const filterStatuses = [
   {
-    statuses: [0],
+    statuses: [
+      OrderStatus.Pending,
+      OrderStatus.Confirmed,
+      OrderStatus.Preparing,
+      OrderStatus.Delivering,
+      OrderStatus.Delivered,
+      OrderStatus.FailDelivery,
+      OrderStatus.IssueReported,
+      OrderStatus.UnderReview,
+      OrderStatus.Completed,
+      OrderStatus.Resolved,
+      OrderStatus.Rejected,
+      OrderStatus.Cancelled,
+    ],
     label: "Tất cả",
   },
   {
@@ -82,7 +101,14 @@ const Order = () => {
     phoneNumber: "",
     pageIndex: 1,
     pageSize: 100_000_000,
+    startTime: 0,
+    endTime: 2400,
+    intendedRecieveDate: "2024/10/18",
+    // intendedRecieveDate: new Date()
+    //   .toLocaleDateString("sv-SE")
+    //   .replace(/-/g, "/"),
   } as OrderFetchQuery);
+
   const {
     data: orderFetchData,
     isLoading: isOrderFetchingLoading,
@@ -90,13 +116,27 @@ const Order = () => {
     refetch: orderFetchRefetch,
   } = useFetchWithRQWithFetchFunc(
     REACT_QUERY_CACHE_KEYS.ORDER_LIST,
-    (): Promise<FetchResponse<OrderFetchModel>> =>
-      apiClient.get(endpoints.ORDER_LIST).then((response) => response.data),
+    async (): Promise<FetchResponse<OrderFetchModel>> =>
+      apiClient
+        .get(
+          endpoints.ORDER_LIST +
+            "?" +
+            query.status.map((item) => "status=" + item).join("&"),
+          {
+            headers: {
+              Authorization: `Bearer ${await sessionService.getAuthToken()}`,
+            },
+            params: {
+              ...query,
+            },
+          }
+        )
+        .then((response) => response.data),
     [query]
   );
-  useEffect(() => {
-    console.log(orderFetchData);
-  }, [isOrderFetchingLoading]);
+  // useEffect(() => {
+  //   console.log("RESPONSE: ", orderFetchData);
+  // }, [orderFetchData, query]);
 
   return (
     <View className="w-full h-full bg-white text-black p-4 relative">
@@ -126,6 +166,12 @@ const Order = () => {
           />
         </View>
         <ScrollView style={{ width: "100%", flexShrink: 0 }} horizontal={true}>
+          {isOrderFetchingLoading && (
+            <ActivityIndicator
+              animating={isOrderFetchingLoading}
+              color="#FCF450"
+            />
+          )}
           <View className="w-full flex-row gap-2 items-center justify-between pb-2">
             {filterStatuses.map((filter, index) => (
               <TouchableOpacity
@@ -145,22 +191,31 @@ const Order = () => {
         </ScrollView>
         <ScrollView style={{ width: "100%", flexGrow: 1 }}>
           <View className="gap-y-2 pb-[154px]">
-            {sampleOrderFetchList.map((order) => (
+            {orderFetchData?.value.items.map((order) => (
               <View
                 key={order.id}
                 className="p-4 pt-3 bg-white border-2 border-gray-300 rounded-lg"
               >
                 <View className="flex-row items-center justify-between gap-2">
-                  <Text className="text-md font-psemibold">MS-{order.id}</Text>
-                  <View className="flex-row gap-2 items-start">
+                  <View className="flex-row items-center">
+                    <Text className="text-[12px] font-psemibold bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-400 dark:text-dark-100">
+                      MS-{order.id}
+                    </Text>
+                  </View>
+                  <View className="flex-row gap-x-1 items-center">
                     {/* <TouchableOpacity
                       onPress={() => {}}
                       className="bg-white border-[#227B94] border-2 rounded-md items-center justify-center px-[4px] py-[2.2px]"
                     >
                       <Text className="text-[13.5px]">Chi tiết</Text>
                     </TouchableOpacity> */}
+                    <Text className="ml-2  font-psemibold bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-200 dark:text-blue-500 text-[12px] rounded">
+                      {formatTime(order.startTime) +
+                        " - " +
+                        formatTime(order.endTime)}
+                    </Text>
                     <Text
-                      className={`text-sm font-medium me-2 px-2.5 py-0.5 rounded ${
+                      className={`text-[12px] font-medium me-2 px-2.5 py-1 rounded ${
                         getOrderStatusDescription(order.status)?.bgColor
                       } `}
                     >
@@ -168,7 +223,7 @@ const Order = () => {
                     </Text>
                   </View>
                 </View>
-                <View className="mt-4 gap-1">
+                <View className="mt-4 gap-1 pt-1">
                   <View className="flex-row justify-start items-center gap-2">
                     <Image
                       source={{
