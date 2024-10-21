43841a4081fd796beb0592ce9a122f897697f81d
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-10-21 15:43:12 +0700

    Update OrderDetail.tsx

--- components/order/OrderDetail.tsx
+++ components/order/OrderDetail.tsx
@@ -12,6 +12,17 @@ const formatTime = (time: number): string => {
   const minutes = (time % 100).toString().padStart(2, "0");
   return `${hours}:${minutes}`;
 };
+function maskPhoneNumber(phoneNumber: string): string {
+  if (phoneNumber.length <= 6) {
+    return phoneNumber;
+  }
+
+  const start = phoneNumber.slice(0, 3);
+  const end = phoneNumber.slice(-3);
+  const masked = "*".repeat(phoneNumber.length - 6);
+
+  return `${start}${masked}${end}`;
+}
 const formatDate = (dateString: string): string => {
   const date = new Date(dateString.replace(/\//g, "-"));
   return date.toLocaleDateString("en-GB");
@@ -48,25 +59,25 @@ const OrderDetail = ({
 
   return (
     <View
-      className={`items-center justify-center w-full ${containerStyleClasses} bg-[#f4f4f5] py-2`}
+      className={`items-center justify-center w-full bg-[#f4f4f5] py-2 ${containerStyleClasses}`}
     >
       {isLoading ? (
         <ActivityIndicator color="#FCF450" />
       ) : (
         <View className="w-full">
-          <View className="px-2 gap-y-2">
+          <View className="px-2 gap-y-3">
             <View className="flex-row items-center justify-between">
-              <Text className="text-[12px] text-gray-800 font-semibold">
+              <Text className="text-[12.5px] text-gray-800 font-semibold mt-1">
                 Đơn hàng MS-{order.id}
               </Text>
               <View className="flex-row gap-x-1 items-center">
-                <Text className="ml-2  font-psemibold bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-200 dark:text-blue-500 text-[9px] rounded">
+                <Text className="ml-2  font-psemibold bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-200 dark:text-blue-500 text-[10px] rounded">
                   {formatTime(order.startTime) +
                     " - " +
                     formatTime(order.endTime)}
                 </Text>
                 <Text
-                  className={`text-[9px] font-medium me-2 px-2.5 py-1 rounded ${
+                  className={`text-[10px] font-medium me-2 px-2.5 py-1 rounded ${
                     getOrderStatusDescription(order.status)?.bgColor
                   }`}
                 >
@@ -75,7 +86,7 @@ const OrderDetail = ({
               </View>
             </View>
             <View>
-              <Text className="text-md text-gray-500 italic">
+              <Text className="text-[10px] text-gray-500 italic">
                 {order.customer.fullName} đã đặt vào{" "}
                 {new Date(order.orderDate).toLocaleTimeString("vi-VN", {
                   hour: "2-digit",
@@ -89,17 +100,38 @@ const OrderDetail = ({
             <Text className="text-[15px] text-gray-600 font-semibold">
               Thông tin nhận hàng
             </Text>
-
-            <View>
+            <View className="mt-3 border-gray-300 border-[0.5px]" />
+            <View className="py-2">
               <Text className="text-[14px] text-gray-700 font-semibold">
-                {order.buildingName}
+                {order.customer.fullName}
               </Text>
               <Text className="text-[14px] text-gray-700 font-semibold">
+                {maskPhoneNumber(order.customer.phoneNumber)}
+              </Text>
+              <Text className="text-[14px] text-gray-700 font-semibold italic">
                 {order.buildingName}
               </Text>
             </View>
+            <View className="mt-1 border-gray-300 border-[0.3px]" />
+            <View className="py-2 ">
+              <Text className="text-[14px] text-gray-700">
+                Khung nhận hàng:
+              </Text>
+              <Text className="text-[14px] text-gray-700 font-semibold">
+                {formatDate(order.intendedReceiveDate) +
+                  " | " +
+                  formatTime(order.startTime) +
+                  " - " +
+                  formatTime(order.endTime)}
+              </Text>
+            </View>
           </View>
-          <View className="mt-3 border-gray-300 border-[0.5px]" />
+          <View className="mt-4 bg-white p-2">
+            <Text className="text-[15px] text-gray-600 font-semibold">
+              Chi tiết đơn hàng
+            </Text>
+            <View className="mt-3 border-gray-300 border-[0.5px]" />
+          </View>
         </View>
       )}
     </View>
