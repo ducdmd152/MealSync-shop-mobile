3296406755981b5274d1db399ab80e65cc05c16b
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-11-09 09:20:05 +0700

    Update AvatarChange.tsx

--- components/common/AvatarChange.tsx
+++ components/common/AvatarChange.tsx
@@ -8,8 +8,11 @@ import { ShopProfileGetModel } from "@/types/models/ShopProfileModel";
 import { FetchValueResponse } from "@/types/responses/FetchResponse";
 import * as ImagePicker from "expo-image-picker";
 import React, { useEffect, useState } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Alert, Platform, StyleSheet, Text, View } from "react-native";
 import { Avatar, Button, IconButton } from "react-native-paper";
+import CustomButton from "../custom/CustomButton";
+import { unSelectLocation } from "@/hooks/states/useMapLocationState";
+import Toast from "react-native-toast-message";
 
 const styles = StyleSheet.create({
   shadow: {
@@ -38,10 +41,10 @@ const AvatarChange: React.FC<AvatarChangeProps> = () => {
     []
   );
   const [user, setUser] = useState({
-    avatarUrl: CONSTANTS.url.shopAvatarSample,
+    avatarUrl: shopProfile.data?.value.logoUrl || CONSTANTS.url.avatar,
     fullname: shopProfile.data?.value.name || "--------------",
   });
-  const [isChangeMode, setIsChangeMode] = useState(false);
+  const [isChangeMode, setIsChangeMode] = useState(true);
   const [avatar, setAvatar] = useState(CONSTANTS.url.avatar);
 
   useEffect(() => {
@@ -50,8 +53,15 @@ const AvatarChange: React.FC<AvatarChangeProps> = () => {
     }
   }, [user]);
   useEffect(() => {
-    setAvatar(shopProfile.data?.value.logoUrl || CONSTANTS.url.avatar);
+    if (shopProfile.isFetched && !isChangeMode)
+      setUser({
+        avatarUrl: shopProfile.data?.value.logoUrl || CONSTANTS.url.avatar,
+        fullname: shopProfile.data?.value.name || "------",
+      });
   }, [shopProfile.data?.value]);
+  useEffect(() => {
+    shopProfile.refetch();
+  }, [isChangeMode]);
 
   const pickImage = async () => {
     if (Platform.OS !== "web") {
@@ -71,7 +81,7 @@ const AvatarChange: React.FC<AvatarChangeProps> = () => {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
@@ -79,37 +89,80 @@ const AvatarChange: React.FC<AvatarChangeProps> = () => {
 
     if (!result.canceled && result.assets) {
       setAvatar(result.assets[0].uri);
+      handleSaveAvatar(result.assets[0].uri);
     }
   };
 
-  const handleSaveAvatar = async () => {
+  const handleSaveAvatar = async (imageURI: string) => {
     try {
-      //   const formData = new FormData();
-      //   formData.append("avatarImageFile", {
-      //     uri: avatar,
-      //     type: "image/jpg",
-      //     name: "avatar.jpg",
-      //   });
-      //   const res = await api.put(
-      //     `/api/v1/customer/upload/${user?.id}`,
-      //     formData,
-      //     {
-      //       headers: {
-      //         "Content-Type": "multipart/form-data",
-      //       }
-      //     },
-      //   );
-      //   const data = await res.data;
-      //   if (data.isSuccess) {
-      //     // dispatch(loaduser());
-      //   }
-    } catch (error) {
-      console.error("Error uploading avatar:", error);
+      if (!imageURI) {
+        Alert.alert("Lỗi", "Không có ảnh nào được chọn.");
+        return;
+      }
+
+      const response = await fetch(imageURI);
+      const blob = await response.blob();
+      console.log("blob.size: ", blob.size);
+
+      // Check if file size is within limit (5 MB in bytes)
+      const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
+      if (blob.size > MAX_FILE_SIZE) {
+        Alert.alert("Lỗi", "Ảnh vượt quá dung lượng cho phép 5 MB.");
+        return;
+      }
+
+      const extension = blob.type === "image/png" ? "png" : "jpg";
+      const fileName = `avatar.${extension}`;
+
+      const file = new File([blob], fileName, { type: blob.type });
+      const formData = new FormData();
+      // formData.append("file", file);
+      formData.append("file", blob, fileName);
+
+      // Upload the image
+      const res = await apiClient.put("storage/file/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const data = res.data as { value: { url: string } };
+      console.log("res.data: ", res.data);
+      await apiClient
+        .put("shop-owner/logo/update", {
+          logoUrl: data.value.url,
+        })
+        .then((res) => {
+          console.log(
+            "data.value.url: ",
+            data.value.url,
+            res.data?.value?.logoUrl
+          );
+        });
+
+      await shopProfile.refetch();
+      Toast.show({
+        type: "success",
+        text1: "Hoàn tất",
+        text2: "Cập nhật ảnh đại diện cửa hàng thành công!",
+      });
+    } catch (error: any) {
+      setAvatar(user.avatarUrl);
+      console.log(error, error?.response?.data);
+      Alert.alert(
+        "Oops!",
+        error?.response?.data?.error?.message ||
+          error?.response?.data?.value?.File[0] ||
+          "Yêu cầu bị từ chối, vui lòng thử lại sau!"
+      );
     }
   };
 
   return (
-    <View style={{ alignItems: "center", marginTop: 32 }}>
+    <View
+      style={{ alignItems: "center", marginTop: 32 }}
+      className={`${isChangeMode && "mb-3"}`}
+    >
       <View
         style={{
           ...styles.shadow,
@@ -131,32 +184,31 @@ const AvatarChange: React.FC<AvatarChangeProps> = () => {
         {user?.fullname || "Khách hàng"}
       </Text>
 
-      {isChangeMode ? (
+      {/* {!isChangeMode ? (
         <View style={{ flexDirection: "row", marginTop: 8, gap: 8 }}>
-          <Button
-            mode="outlined"
-            onPress={() => {
+          <CustomButton
+            title="Lưu ảnh"
+            containerStyleClasses="bg-white  bg-[#227B94] h-7"
+            textStyleClasses="text-[14px] text-white"
+            handlePress={() => {
+              handleSaveAvatar();
+            }}
+          />
+          <CustomButton
+            title="Hủy"
+            containerStyleClasses="bg-white  border-[2px] boder-[#227B94] h-7 border-gray-300"
+            textStyleClasses="text-[14px] text-[#227B94] mt-[-1.5px]"
+            handlePress={() => {
               setAvatar(user?.avatarUrl || CONSTANTS.url.avatar);
               setIsChangeMode(false);
             }}
-          >
-            Hủy
-          </Button>
-          <Button
-            mode="contained"
-            onPress={() => {
-              handleSaveAvatar();
-              setIsChangeMode(false);
-            }}
-          >
-            Lưu ảnh
-          </Button>
+          />
         </View>
       ) : (
-        <Button onPress={() => setIsChangeMode(true)}>
+        <Button onPress={() => setIsChangeMode(true)} textColor="#a78bfa">
           Chỉnh sửa hình ảnh
         </Button>
-      )}
+      )} */}
     </View>
   );
 };
