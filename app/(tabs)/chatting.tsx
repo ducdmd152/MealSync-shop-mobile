e5e9c53f7ccc029907b08c0833bf7b27ea3dc14e
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-11-21 09:32:02 +0700

    Chat channel list ui

--- app/(tabs)/chatting.tsx
+++ app/(tabs)/chatting.tsx
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Button,
-  ScrollView,
-  Alert,
-  SafeAreaView,
-} from "react-native";
-import FetchResponse from "../../types/responses/FetchResponse";
-import TestModel from "../../types/models/TestModel";
-import apiClient from "../../services/api-services/api-client";
-import { endpoints } from "../../services/api-services/api-service-instances";
-import useCounterState from "@/hooks/states/useCounterState";
-import useFetchWithRQWithFetchFunc from "@/hooks/fetching/useFetchWithRQWithFetchFunc";
-import REACT_QUERY_CACHE_KEYS from "@/constants/react-query-cache-keys";
-import ScrollPicker from "react-native-wheel-scrollview-picker";
-import useGlobalHeaderPage from "@/hooks/states/useGlobalHeaderPage";
-import { router, useFocusEffect } from "expo-router";
-import PubNub from "pubnub";
-import { PubNubProvider } from "pubnub-react";
-import { Chat, MemberList } from "@pubnub/react-native-chat-components";
 import Chats from "@/components/realtime/Chats";
+import useGlobalHeaderPage from "@/hooks/states/useGlobalHeaderPage";
+import { useFocusEffect } from "expo-router";
+import React from "react";
+import { SafeAreaView, View } from "react-native";
 
 const Chatting = () => {
   const globalHeaderPage = useGlobalHeaderPage();
@@ -35,7 +17,7 @@ const Chatting = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View className="pt-3 bg-white flex-1">
+      <View className="pt-2 bg-white flex-1">
         <Chats />
       </View>
     </SafeAreaView>
--- components/realtime/Chats.tsx
+++ components/realtime/Chats.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button, ScrollView, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ScrollView,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import FetchResponse from "../../types/responses/FetchResponse";
 import TestModel from "../../types/models/TestModel";
 import apiClient from "../../services/api-services/api-client";
@@ -10,171 +17,34 @@ import REACT_QUERY_CACHE_KEYS from "@/constants/react-query-cache-keys";
 import ScrollPicker from "react-native-wheel-scrollview-picker";
 import useGlobalHeaderPage from "@/hooks/states/useGlobalHeaderPage";
 import { router, useFocusEffect } from "expo-router";
-import PubNub from "pubnub";
-import { PubNubProvider } from "pubnub-react";
-import { Chat, MemberList } from "@pubnub/react-native-chat-components";
-const members = [
-  {
-    name: "Mark Kelley",
-    custom: {
-      title: "Office Assistant",
-    },
-    email: null,
-    eTag: "AYGyoY3gre71eA",
-    externalId: null,
-    id: "user_63ea15931d8541a3bd35e5b1f09087dc",
-    profileUrl: "https://randomuser.me/api/portraits/men/1.jpg",
-    updated: "2020-09-23T09:23:34.598494Z",
-  },
-  {
-    name: "Anna Gordon",
-    custom: {
-      title: "VP Marketing",
-    },
-    email: null,
-    eTag: "AZDyqJ7andTHlAE",
-    externalId: null,
-    id: "user_3c4400761cba4b65b77b6cae55fc21eb",
-    profileUrl: "https://randomuser.me/api/portraits/women/1.jpg",
-    updated: "2020-09-23T09:23:33.598365Z",
-  },
-  {
-    name: "Luis Griffin",
-    custom: {
-      title: "Environmental Tech",
-    },
-    email: null,
-    eTag: "AeTqgs2X16ql2wE",
-    externalId: null,
-    id: "user_def709b1adfc4e67b98bb7a820f581b1",
-    profileUrl: "https://randomuser.me/api/portraits/men/2.jpg",
-    updated: "2020-09-23T09:23:31.809198Z",
-  },
-  {
-    name: "Sue Flores",
-    custom: {
-      title: "VP Sales",
-    },
-    email: null,
-    eTag: "Acmf1d+vmczaKw",
-    externalId: null,
-    id: "user_a56c20222c484ff8987ec9b69b0c8f5b",
-    profileUrl: "https://randomuser.me/api/portraits/women/2.jpg",
-    updated: "2020-09-23T09:23:31.837372Z",
-  },
-  {
-    name: "Luke Young",
-    custom: {
-      title: "Product Engineer",
-    },
-    email: null,
-    eTag: "AczQlJSt5Zn9jAE",
-    externalId: null,
-    id: "user_e0e43601f93249c382415521188f0208",
-    profileUrl: "https://randomuser.me/api/portraits/men/3.jpg",
-    updated: "2020-09-23T09:23:36.617721Z",
-  },
-  {
-    name: "Jenny Porter",
-    custom: {
-      title: "Engineer",
-    },
-    email: null,
-    eTag: "AZSThaqOxP67Rg",
-    externalId: null,
-    id: "user_ed145d8b0f094b83a2d51d428a59d0cb",
-    profileUrl: "https://randomuser.me/api/portraits/women/3.jpg",
-    updated: "2020-09-23T09:23:32.810646Z",
-  },
-  {
-    name: "Sue Jones",
-    custom: {
-      title: "Desktop Support Technician",
-    },
-    email: null,
-    eTag: "AZi01cCxr8eo5AE",
-    externalId: null,
-    id: "user_912210948aa849eda2c3e0c2b58355e6",
-    profileUrl: "https://randomuser.me/api/portraits/women/10.jpg",
-    updated: "2020-09-23T09:23:34.54631Z",
-  },
-  {
-    name: "Marie Harper",
-    custom: {
-      title: "Technical Writer",
-    },
-    email: null,
-    eTag: "Ad2mzoKft7HsWQ",
-    externalId: null,
-    id: "user_0368d27f4d514079bc5cfd5678ec1fe7",
-    profileUrl: "https://randomuser.me/api/portraits/women/12.jpg",
-    updated: "2020-09-23T09:23:32.180254Z",
-  },
-  {
-    name: "Tom Martinez",
-    custom: {
-      title: "Product Engineer",
-    },
-    email: null,
-    eTag: "Ad/X1fzkiOTFzwE",
-    externalId: null,
-    id: "user_83647bfb6bcd412d93b1e57e1b602d3a",
-    profileUrl: "https://randomuser.me/api/portraits/men/13.jpg",
-    updated: "2020-09-23T09:23:31.848064Z",
-  },
-  {
-    name: "Veronica Barrett",
-    custom: {
-      title: "Registered Nurse",
-    },
-    email: null,
-    eTag: "AZuL1I+IxJPm4gE",
-    externalId: null,
-    id: "user_4ec689d24845466e93ee358c40358473",
-    profileUrl: "https://randomuser.me/api/portraits/women/15.jpg",
-    updated: "2020-09-23T09:23:31.819548Z",
-  },
-];
-const pubnub = new PubNub({
-  publishKey: "demo",
-  subscribeKey: "demo",
-  userId: "user_a56c20222c484ff8987ec9b69b0c8f5b",
-});
-const currentChannel = "Default";
-const theme = "light";
+import { Avatar } from "react-native-paper";
+import CONSTANTS from "@/constants/data";
 const Chats = () => {
   return (
-    <PubNubProvider client={pubnub}>
-      <Chat {...{ currentChannel, theme }}>
-        <MemberList
-          members={members}
-          presentMembers={[
-            "user_3c4400761cba4b65b77b6cae55fc21eb",
-            "user_def709b1adfc4e67b98bb7a820f581b1",
-            "user_a56c20222c484ff8987ec9b69b0c8f5b",
-          ]}
-          // selfText="(Me)"
-          // sort={(m1, m2) => (m1.name > m2.name ? -1 : 1)}
-          // extraActionsRenderer={(m) => (
-          //   <Button title="Info" onPress={() => alert(`Info about ${m.name}`)} />
-          // )}
-          // memberRenderer={(m) => <Text>{m.name + (m.profileUrl || "")}</Text>}
-          onMemberClicked={(member) => {
-            router.push("/chats/chatbox");
-          }}
-          // onMemberLongPressed={(m) => alert(`Long pressed: ${m.name}`)}
-          style={{
-            memberPressed: {
-              backgroundColor: "#f0fdfa",
-            },
-            member: {
-              borderWidth: 0.2,
-              borderColor: "#cffafe",
-            },
-          }}
-        />
-      </Chat>
-    </PubNubProvider>
+    <ScrollView style={{ flexGrow: 1 }}>
+      {Array.from({ length: 20 }, (_, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => {}}
+          className={`p-3 px-4 bg-white border-b-[0.5px] border-gray-200 flex-row`}
+          style={{ backgroundColor: index < 3 ? "#fffbeb" : "#fff" }}
+        >
+          <Avatar.Image
+            size={40}
+            source={{
+              uri: CONSTANTS.url.avatarDefault,
+            }}
+          />
+          <View className="ml-3 gap-y-1">
+            <Text className="font-medium">MS-100</Text>
+            <Text className="text-[11px]">
+              Shipper: tôi đang đến{" "}
+              <Text className="italic text-[9px]"> 11:20</Text>
+            </Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
   );
 };
 
