041fa5c2c2d7f3c0bf10230ed9d18ac1ece62d54
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-11-22 16:04:10 +0700

    inifinite loading for notifications page

--- components/realtime/Notifications.tsx
+++ components/realtime/Notifications.tsx
@@ -6,7 +6,7 @@ import {
   ScrollView,
   RefreshControl,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CONSTANTS from "@/constants/data";
 import dayjs from "dayjs";
 import { router, useFocusEffect } from "expo-router";
@@ -21,6 +21,8 @@ import FetchResponse from "@/types/responses/FetchResponse";
 import useGlobalNotiState from "@/hooks/states/useGlobalNotiState";
 import useOrderDetailPageState from "@/hooks/states/useOrderDetailPageState";
 import OrderDetailModel from "@/types/models/OrderDetailModel";
+import { ActivityIndicator } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 interface NotiModel {
   id: number;
   accountId: number;
@@ -36,10 +38,13 @@ interface NotiModel {
 enum EntityTypes {
   Order = 1,
 }
+const INFINITE_LOAD_SIZE = 10;
 const Notifications = () => {
   const globalHeaderPage = useGlobalHeaderPage();
   const globalNotiState = useGlobalNotiState();
   const globalOrderDetailPageState = useOrderDetailPageState();
+  const [infiniteSize, setInfitieSize] = useState(INFINITE_LOAD_SIZE);
+  const [isRendering, setIsRendering] = useState(false);
   const notiFetcher = useFetchWithRQWithFetchFunc(
     [endpoints.NOTIFICATION_LIST],
     async (): Promise<FetchResponse<NotiModel>> =>
@@ -47,14 +52,14 @@ const Notifications = () => {
         .get(endpoints.NOTIFICATION_LIST, {
           params: {
             pageIndex: 1,
-            pageSize: 100_000_000,
+            pageSize: infiniteSize,
           },
         })
         .then((response) => response.data),
-    []
+    [infiniteSize]
   );
   useEffect(() => {
-    if (globalHeaderPage.isNotiPageFocusing) notiFetcher.refetch();
+    if (globalHeaderPage.isNotiPageFocusing) setInfitieSize(INFINITE_LOAD_SIZE);
     // console.log(
     //   "globalHeaderPage.isChattingFocusing: ",
     //   globalHeaderPage.isNotiPageFocusing
@@ -64,6 +69,14 @@ const Notifications = () => {
     if (globalHeaderPage.isNotiPageFocusing) notiFetcher.refetch();
   }, [globalNotiState.toggleChangingFlag]);
 
+  useEffect(() => {
+    if (notiFetcher.isFetched) {
+      setIsRendering(true);
+      setTimeout(() => {
+        setIsRendering(false);
+      }, 1500);
+    }
+  }, [notiFetcher.isFetched]);
   useFocusEffect(
     React.useCallback(() => {
       globalHeaderPage.setIsNotiPageFocusing(true);
@@ -72,7 +85,95 @@ const Notifications = () => {
       };
     }, [])
   );
+  const renderItem = ({ item }: { item: NotiModel }) => (
+    <TouchableOpacity
+      key={item.id}
+      onPress={() => {
+        if (item.entityType == EntityTypes.Order) {
+          globalOrderDetailPageState.setOrder({} as OrderDetailModel);
+          globalOrderDetailPageState.setId(item.referenceId);
+          router.push("/order/details");
+        }
+      }}
+      className={`p-3 px-4 bg-white border-b-[0.5px] border-gray-200 `}
+      style={{ backgroundColor: item.isRead ? "#fff" : "#fffbeb" }}
+    >
+      <View className="flex-row flex-1 justify-start items-start">
+        <View className="self-start border-[1px] border-gray-200 mr-2 ml-[-2px] rounded-full p-[1px] overflow-hidden mb-1">
+          <Image
+            source={{
+              uri: CONSTANTS.url.nofi_icon,
+            }}
+            resizeMode="cover"
+            className="h-[40px] w-[40px] opacity-85"
+          />
+        </View>
+        <View className="flex-1">
+          <Text
+            className="text-[14px] font-semibold mt-[-2px]"
+            numberOfLines={2}
+            ellipsizeMode="tail"
+          >
+            {item.title +
+              (item.entityType == EntityTypes.Order
+                ? ` MS-${item.referenceId}`
+                : "")}
+          </Text>
+          <Text
+            className="text-[12px] italic mt-[2px] mb-[2px]"
+            numberOfLines={2}
+            ellipsizeMode="tail"
+          >
+            {item.content}
+          </Text>
+          <View className="flex-row justify-between">
+            <View>
+              <Text className="text-[11px] italic text-gray-500 ">
+                {dayjs(item.createdDate).local().format("HH:mm - DD/MM/YYYY")}{" "}
+              </Text>
+              {/* <Text className="text-[11px] italic text-gray-500 ">
+              Số dư sau giao dịch:{" "}
+              {utilService.formatPrice(transaction.totalAmountAfter)}
+              {"₫"}
+            </Text> */}
+            </View>
+            {/* <Text
+            className={`text-[11px] italic text-gray-500 ${
+              transaction.amount > 0 ? "text-green-600" : "text-red-700"
+            } font-semibold`}
+          >
+            {transaction.amount > 0 ? "+" : "-"}
+            {utilService.formatPrice(Math.abs(transaction.amount))}
+            {"₫"}
+          </Text> */}
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 
+  return (
+    <FlatList
+      style={{ flexGrow: 1, backgroundColor: "white" }}
+      data={notiFetcher.data?.value.items || []} // Danh sách dữ liệu
+      renderItem={renderItem} // Hàm render phần tử
+      keyExtractor={(item, index) => item.id.toString()} // Khóa duy nhất
+      onEndReached={() => {
+        if (notiFetcher.isFetching || isRendering) return;
+        setInfitieSize(infiniteSize + INFINITE_LOAD_SIZE);
+      }} // Gọi khi cuộn đến cuối
+      onEndReachedThreshold={0.5} // Ngưỡng trước khi gọi tải thêm (0.5 = 50% cuối)
+      ListFooterComponent={
+        <View className="p-3">
+          <ActivityIndicator
+            animating={notiFetcher.isFetching || isRendering}
+            color="#FCF450"
+            size={40}
+          />
+        </View>
+      } // Phần footer loading
+    />
+  );
   return (
     <ScrollView
       style={{ flexGrow: 1 }}
@@ -133,25 +234,31 @@ const Notifications = () => {
                       .format("HH:mm - DD/MM/YYYY")}{" "}
                   </Text>
                   {/* <Text className="text-[11px] italic text-gray-500 ">
-                    Số dư sau giao dịch:{" "}
-                    {utilService.formatPrice(transaction.totalAmountAfter)}
-                    {"₫"}
-                  </Text> */}
-                </View>
-                {/* <Text
-                  className={`text-[11px] italic text-gray-500 ${
-                    transaction.amount > 0 ? "text-green-600" : "text-red-700"
-                  } font-semibold`}
-                >
-                  {transaction.amount > 0 ? "+" : "-"}
-                  {utilService.formatPrice(Math.abs(transaction.amount))}
+                  Số dư sau giao dịch:{" "}
+                  {utilService.formatPrice(transaction.totalAmountAfter)}
                   {"₫"}
                 </Text> */}
+                </View>
+                {/* <Text
+                className={`text-[11px] italic text-gray-500 ${
+                  transaction.amount > 0 ? "text-green-600" : "text-red-700"
+                } font-semibold`}
+              >
+                {transaction.amount > 0 ? "+" : "-"}
+                {utilService.formatPrice(Math.abs(transaction.amount))}
+                {"₫"}
+              </Text> */}
               </View>
             </View>
           </View>
         </TouchableOpacity>
       ))}
+      <View className="min-h-20 bg-gray-200">
+        <ActivityIndicator
+          animating={notiFetcher.isFetching || isRendering}
+          color="#FCF450"
+        />
+      </View>
     </ScrollView>
   );
 };
