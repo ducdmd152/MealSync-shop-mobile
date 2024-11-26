5e09971978d86c0133d9db55d1bdc229744ce3df
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-11-26 13:59:22 +0700

    Update create.tsx

--- app/(pages)/withdrawal/create.tsx
+++ app/(pages)/withdrawal/create.tsx
@@ -422,7 +422,7 @@ const WithdrawalCreate = () => {
                 <Text className="font-bold">Tên chủ tài khoản *</Text>
                 <TextInput
                   className="border border-gray-300 mt-1 p-2 px-3 rounded text-[20px]"
-                  placeholder="Nhập số tài khoản..."
+                  placeholder="Nhập tên chủ tài khoản..."
                   keyboardType="numeric"
                   onFocus={() => setIsUnderKeywodFocusing(true)}
                   onBlur={() => setIsUnderKeywodFocusing(false)}
