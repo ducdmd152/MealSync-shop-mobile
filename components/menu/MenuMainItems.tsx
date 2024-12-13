b48d7dc52bef269c5ed19517cb9e0d01cd6fba9d
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-12-13 22:57:45 +0700

    Add "Thay đổi thứ tự" button

--- components/menu/MenuMainItems.tsx
+++ components/menu/MenuMainItems.tsx
@@ -25,6 +25,7 @@ import {
   TouchableOpacity,
   View,
   RefreshControl,
+  ActivityIndicator,
 } from "react-native";
 import Collapsible from "react-native-collapsible";
 import DraggableFlatList, {
@@ -523,7 +524,7 @@ const MenuMainItems = ({ beforeGo }: { beforeGo: () => void }) => {
         />
       </View>
 
-      <View className="w-full gap-2 p-4 pt-3">
+      <View className="w-full gap-1 p-4 pt-3">
         <View className="w-full">
           {/* <Searchbar
             style={{
@@ -538,6 +539,7 @@ const MenuMainItems = ({ beforeGo }: { beforeGo: () => void }) => {
             value={searchQuery}
           /> */}
         </View>
+
         <ScrollView style={{ width: "100%", flexShrink: 0 }} horizontal={true}>
           <View className="w-full flex-row gap-2 items-center justify-between pb-2 ">
             <TouchableOpacity
@@ -608,16 +610,21 @@ const MenuMainItems = ({ beforeGo }: { beforeGo: () => void }) => {
             </TouchableOpacity>
           </View>
         </ScrollView>
+        <TouchableOpacity onPress={() => {}}>
+          <Text className="text-right text-[12.5px] mt-[-2px] mb-[-4px] text-white text-[#227B94] font-semibold">
+            Thay đổi thứ tự
+          </Text>
+        </TouchableOpacity>
         <View className="gap-y-2 pb-[272px]">
-          {/* {isLoading && (
-            <ActivityIndicator animating={isLoading} color="#FCF450" />
-          )} */}
+          {isFetching && (
+            <ActivityIndicator animating={isFetching} color="#FCF450" />
+          )}
           {!isFetching && !categories?.value.length && (
             <Text className="text-gray-600 text-center mt-[-12px]">
               Không tìm thấy danh mục và sản phẩm tương ứng
             </Text>
           )}
-          <DraggableFlatList
+          {/* <DraggableFlatList
             style={{ width: "100%", flexGrow: 1 }}
             data={extendCategories}
             renderItem={renderCategory}
@@ -634,7 +641,242 @@ const MenuMainItems = ({ beforeGo }: { beforeGo: () => void }) => {
                 refreshing={isLoading}
               />
             }
-          />
+          /> */}
+
+          <ScrollView
+            style={{ flexGrow: 1 }}
+            refreshControl={
+              <RefreshControl
+                tintColor={"#FCF450"}
+                onRefresh={() => {
+                  refetch();
+                }}
+                refreshing={isFetching}
+              />
+            }
+          >
+            {extendCategories.map((item) => {
+              return (
+                <View key={item.id} className="mb-1">
+                  <TouchableOpacity
+                    className="flex-row justify-between items-center pr-2 mb-2"
+                    onPress={() =>
+                      setExtendCategories(
+                        extendCategories?.map((cat) =>
+                          item.id === cat.id
+                            ? { ...cat, isCollapsible: !cat.isCollapsible }
+                            : cat
+                        )
+                      )
+                    }
+                    // onLongPress={drag}
+                  >
+                    <View>
+                      <View className="flex-row items-center gap-x-2 justify-start">
+                        <Text className="font-bold text-lg text-gray-800">
+                          {item.name}
+                        </Text>
+                        <Text className="text-gray-700 text-sm">
+                          {item.foods?.length} món
+                        </Text>
+                      </View>
+                      <CustomButton
+                        title="Chỉnh sửa danh mục"
+                        handlePress={async () => {
+                          setNotFoundInfo(
+                            notFoundInfo.message,
+                            "/menu",
+                            notFoundInfo.linkDesc
+                          );
+                          try {
+                            const response = await apiClient.get<
+                              ValueResponse<ShopCategoryModel>
+                            >(`shop-owner/category/${item.id}`);
+                            setShopCategoryModel({ ...response.data.value });
+                            beforeGo();
+                            router.push("/menu/category/update");
+                            // console.log("Food Detail model: ", foodDetailModel);
+                          } catch (error: any) {
+                            if (
+                              error.response &&
+                              error.response.status === 404
+                            ) {
+                              Alert.alert(
+                                "Oops!",
+                                "Danh mục này không tồn tại!"
+                              );
+                              refetch();
+                            } else {
+                              Alert.alert(
+                                "Oops!",
+                                error?.response?.data?.error?.message ||
+                                  "Yêu cầu bị từ chối, vui lòng thử lại sau!"
+                              );
+                            }
+                          }
+                        }}
+                        containerStyleClasses="bg-white border-gray-200 border-2 h-[26px] px-[8px] self-start"
+                        textStyleClasses="text-gray-700 text-[10px] mt-[-3.5px] text-[#227B94]"
+                      />
+                    </View>
+                    <View className="flex-row items-center gap-x-6">
+                      {item.isCollapsible ? (
+                        <Ionicons
+                          name="chevron-up-outline"
+                          size={21}
+                          color="gray"
+                        />
+                      ) : (
+                        <Ionicons
+                          name="chevron-down-outline"
+                          size={21}
+                          color="gray"
+                        />
+                      )}
+                    </View>
+                  </TouchableOpacity>
+
+                  <Collapsible collapsed={item.isCollapsible}>
+                    <View className="gap-y-2 pb-2">
+                      {item.foods?.map((food) => (
+                        <View
+                          key={food.id + (Math.random() % 1_00_000_000)}
+                          className="p-4 pt-3 bg-white border-2 border-gray-300 rounded-lg"
+                        >
+                          <View className="flex-row items-start justify-between gap-2">
+                            <View className="flex-row justify-start items-start gap-2 flex-1">
+                              <Image
+                                source={{
+                                  uri:
+                                    food.imageUrl ||
+                                    "https://via.placeholder.com/40", // Fallback image
+                                }}
+                                resizeMode="cover"
+                                className="h-[36px] w-[40px] rounded-md opacity-85"
+                              />
+                              <View className="flex-1">
+                                <Text
+                                  className="text-md font-semibold mt-[-2px] text-gray-600"
+                                  numberOfLines={2}
+                                  ellipsizeMode="tail"
+                                >
+                                  {food.name}
+                                </Text>
+                                <Text className="text-[12.8px] italic text-gray-500 mt-[-2px]">
+                                  {food.price.toLocaleString()}đ
+                                </Text>
+                              </View>
+                            </View>
+
+                            <View className="items-end">
+                              <Switch
+                                className={`scale-75 mt-1 mr-[-6px] ${
+                                  statusingIdList.some((id) => item.id == id)
+                                    ? "opacity-50"
+                                    : ""
+                                } ${
+                                  Platform.OS == "android" ? "mt-[-7px]" : ""
+                                }`}
+                                disabled={statusingIdList.some(
+                                  (id) => item.id == id
+                                )}
+                                color="#e95137"
+                                value={!food.isSoldOut && food.status == 1}
+                                onValueChange={() => onChangeStatus(food)}
+                              />
+                              <Text
+                                className={`ml-2 text-gray-500 italic text-[6px] text-secondary-200 text-gray-500 ${
+                                  Platform.OS == "android"
+                                    ? "mt-[-9px] text-[7px]"
+                                    : ""
+                                }`}
+                              >
+                                {food.isSoldOut
+                                  ? "Tạm hết hàng"
+                                  : food.status == 1
+                                  ? "Đang mở bán"
+                                  : "Tạm ẩn"}
+                              </Text>
+                            </View>
+                          </View>
+
+                          <View className="flex-row justify-between items-center gap-2 pt-2">
+                            <View>
+                              <Text className="text-gray-500 italic text-[12px] text-secondary-200">
+                                {food.totalOrderInNextTwoHours} đơn cần xử lí
+                                trong 2h tới
+                              </Text>
+                              <Text className="mt-1 text-gray-500 italic text-[7px] text-secondary-200 text-gray-500">
+                                {food.operatingSlots.length == 0
+                                  ? "Chưa mở bán khung giờ nào"
+                                  : `Mở bán: ${food.operatingSlots
+                                      .sort((a, b) => a.startTime - b.startTime)
+                                      .map(
+                                        (item) =>
+                                          `${utilService.formatTime(
+                                            item.startTime
+                                          )} - ${utilService.formatTime(
+                                            item.endTime
+                                          )}`
+                                      )
+                                      .join(" | ")}`}
+                              </Text>
+                            </View>
+
+                            <TouchableOpacity
+                              onPress={async () => {
+                                setNotFoundInfo(
+                                  notFoundInfo.message,
+                                  "/menu",
+                                  notFoundInfo.linkDesc
+                                );
+                                try {
+                                  const response = await apiClient.get<
+                                    ValueResponse<FoodDetailModel>
+                                  >(`shop-owner/food/${food.id}/detail`);
+                                  setFoodDetailModel({
+                                    ...response.data.value,
+                                  });
+                                  beforeGo();
+                                  router.push("/menu/food/update");
+                                  // console.log("Food Detail model: ", foodDetailModel);
+                                } catch (error: any) {
+                                  if (
+                                    error.response &&
+                                    error.response.status === 404
+                                  ) {
+                                    Alert.alert(
+                                      "Oops!",
+                                      "Món này không tồn tại!"
+                                    );
+                                    refetch();
+                                  } else {
+                                    Alert.alert(
+                                      "Oops!",
+                                      error?.response?.data?.error?.message ||
+                                        "Hệ thống đang bảo trì, vui lòng thử lại sau!"
+                                    );
+                                  }
+                                }
+                              }}
+                              className="bg-[#227B94] border-[#227B94] border-[1.2px] rounded-md items-center justify-center px-[6px] py-[2px] bg-whitee"
+                            >
+                              {/* text-[#227B94] font-semibold */}
+                              <Text className="text-[12px] text-white ">
+                                Chỉnh sửa
+                              </Text>
+                            </TouchableOpacity>
+                          </View>
+                        </View>
+                      ))}
+                    </View>
+                  </Collapsible>
+
+                  <View className="border-b-2 border-gray-200"></View>
+                </View>
+              );
+            })}
+          </ScrollView>
         </View>
       </View>
 
